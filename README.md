a892b5214851f7eccbfe60b707df65a495bb7a8c
nj1787 <neerajj9@gmail.com>
2024-01-27 22:50:51 +0530

    Native Balance, Transctions and ERC Balance Retrieved.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 ### Blockchain Explorer - Ethereum (Backend)
+
+This is a Blockchain Explorer Application for an Address present on an Ethereum Chain.
+
+### Tech Stack
+
+MongoDB
+Express
+React
+Node
+
+### Hosted Links
+
+## Backend
--- index.js
+++ index.js
@@ -13,23 +13,52 @@ const apiKey = process.env.MORALIS_API_KEY;
 const address = process.env.ETH_ADDRESS;
 const chain = EvmChain.ETHEREUM;
 
+// ROUTES AND CONTROLLERS
+
 app.get("/", (req, res) => {
   res.send("<h1>Welcome To Blockchain Explorer</h1>");
 });
 
-app.get("/demo", async (req, res) => {
-  try {
-    const data = await getDemoData();
-    res.status(200);
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500);
-    res.json({
-      error: error.message,
-    });
-  }
+app.get("/transactions", async (req, res) => {
+  const transactionData = await getTransactions();
+  res.status(200).json(transactionData);
 });
+
+app.get("/native-balance", async (req, res) => {
+  const nativeBalance = await getNativeBalance();
+  res.status(200).json(nativeBalance);
+});
+
+app.get("/erc-token-balance", async (req, res) => {
+  const ercBalance = await getERC20TokensBalance();
+  res.status(200).json(ercBalance);
+});
+
+// UTILITIES
+
+async function getTransactions() {
+  const transactions = await Moralis.EvmApi.transaction.getWalletTransactions({
+    chain,
+    address,
+  });
+  return { transactions };
+}
+
+async function getNativeBalance() {
+  const balance = await Moralis.EvmApi.balance.getNativeBalance({
+    chain,
+    address,
+  });
+  return { balance };
+}
+
+async function getERC20TokensBalance() {
+  const balance = await Moralis.EvmApi.token.getWalletTokenBalances({
+    chain,
+    address,
+  });
+  return { balance };
+}
 
 const startServer = async () => {
   await Moralis.start({
@@ -40,16 +69,5 @@ const startServer = async () => {
     console.log(`Server Running On Port ${port}`);
   });
 };
-
-async function getDemoData() {
-  const nativeBalance = await Moralis.EvmApi.balance.getNativeBalance({
-    address,
-    chain,
-  });
-
-  const native = nativeBalance.result.balance.ether;
-
-  return { native };
-}
 
 startServer();
